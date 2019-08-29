584c19123afbe4b4bf9e5432084245f0361d8b40
Debbie <debbieforbeck@gmail.com>
2019-08-28 22:39:17 -0400

    Up to Join tables

---
+++ 0.07_ALTER_TABLE_COLUMNS.sql
@@ -0,0 +1,19 @@
+﻿-- Add a column called Manufacturer
+ALTER TABLE Products
+	ADD Manufacturer VARCHAR(25) NULL;
+
+--Drop it
+ALTER TABLE Products
+	DROP COLUMN Manufacturer;
+
+-- add UPC column with data type VARCHAR(25)
+ALTER TABLE Products
+	ADD UPC VARCHAR(25) NULL;
+
+--Change UPC to text field
+ALTER TABLE Products
+	ALTER COLUMN UPC TEXT;
+
+-- Drop UPC
+ALTER TABLE Products
+	DROP COLUMN UPC;
---
+++ 0.11__JOIN.sql
@@ -0,0 +1,5 @@
+﻿SET IDENTITY_INSERT Purchases ON
+INSERT Purchases (PurchaseID, ProductID, CustomerID, Quantity) 
+    VALUES (1, 1, 1, 5),
+    (2, 1, 2, 5),
+    (3, 2, 4, 3);
